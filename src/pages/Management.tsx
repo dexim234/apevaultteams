9ea8560490f0bccc0891ded7145a4d02cf835323
dexim234 <alancov87@yandex.ru>
2025-11-20 23:25:22 +0300

    Add files via upload

--- src/pages/Management.tsx
+++ src/pages/Management.tsx
@@ -366,6 +366,7 @@ export const Management = () => {
               </div>
               </div>
             </div>
+          </div>
 
           {/* Participant filter */}
           <div className="mt-4 pt-4 border-t border-gray-700/50 dark:border-gray-700">
